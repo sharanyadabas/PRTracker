652402621a3b92122fe20b02e1b13091a291db21
Sharanya Dabas <sd699@cornell.edu>
2024-07-27 13:30:25 -0400

    Add project files.

---
+++ PRTracker/Entities/Exercise.cs
@@ -0,0 +1,14 @@
+﻿namespace PRTracker.Entities
+{
+    public class Exercise
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Instructions { get; set; }
+        public List<string> Images {  get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? ModifiedDate { get; set; }
+
+    }
+}
