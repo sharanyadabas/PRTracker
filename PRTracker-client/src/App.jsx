81648ac5fb2ec1b49580225300ea950adaea782c
Sharanya Dabas <sd699@cornell.edu>
2024-07-31 17:30:46 -0400

    initialized react front end

---
+++ PRTracker-client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <p>Hello World!</p>
+    </BrowserRouter>
+  );
+}
+
+export default App;
